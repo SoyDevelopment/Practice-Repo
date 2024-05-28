5deb5d928bc7a819764c8ccc1def0a943a54d940
Joevany Martinez <Joevany.g.Martinez@gmail.com>
2024-05-28 21:36:51 +0000

    Add players who can Draw cards

--- src/main.rs
+++ src/main.rs
@@ -37,13 +37,41 @@ impl Deck {
     }
 }
 
+#[derive(Debug)]
+struct Player {
+    name: String,
+    hand: Vec<Card>,
+}
+
+impl Player {
+    fn new(name: &str) -> Player {
+        Player { name: name.to_string(), hand: Vec::new() }
+    }
+
+    fn draw_card(&mut self, deck: &mut Deck) {
+        if let Some(card) = deck.draw() {
+            self.hand.push(card);
+        }
+    }
+}
 fn main() {
     let mut deck = Deck::new();
     deck.shuffle();
 
-    while let Some(card) = deck.draw() {
-        println!("Card: {:?}", card);
+    let mut players = vec![
+        Player::new("Niko"),
+        Player::new("Geo"),
+        Player::new("Katy"),
+        Player::new("Emerson"),
+    ];
+
+    for player in &mut players {
+        for _ in 0..4 {
+            player.draw_card(&mut deck);
+        }
     }
 
-    println!("Deck is empty!");
+    for player in &players {
+        println!("{:?}'s hand: {:?}", player.name, player.hand);
+    }
 }
