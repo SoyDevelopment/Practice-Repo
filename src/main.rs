2faa7426e4bee238928b8d39e65e5b5e73b5e719
Joevany Martinez <Joevany.g.Martinez@gmail.com>
2024-05-28 20:02:08 +0000

    Use Cargo init Don't use Rustc Use Cargo Run

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use rand::seq::SliceRandom;
+extern crate rand;
+
+#[derive(Debug)]
+struct Card {
+    value: i8
+}
+
+impl Card {
+    fn new(num: i8) -> Card {
+        Card{value: num}
+    }
+}
+
+#[derive(Debug)]
+struct Deck {
+    stack: Vec<Card>
+}
+
+impl Deck {
+    fn new() -> Deck {
+        let mut cards = Vec::new();
+        for x in 0..4 { 
+            for _i in 0..10{
+                let new_card = Card::new(x);
+                cards.push(new_card);
+            }
+        }
+        Deck { stack: cards }
+    }
+
+    fn shuffle(&mut self) {
+        self.stack.shuffle(&mut rand::thread_rng());
+    }
+}
+
+fn main() {
+    let mut deck: Deck = Deck::new();
+    deck.shuffle();
+    for card in deck.stack.iter() {
+        println!("Card: {:?}", card);
+    }
+}
