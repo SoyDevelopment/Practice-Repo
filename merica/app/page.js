3c6d45932bac77cb43406e630c1d84795fb03f29
soydevelopment <joevany.g.martinez@gmail.com>
2024-06-20 00:37:35 -0400

    Start Email Fortmat test

--- merica/app/page.js
+++ merica/app/page.js
@@ -1,14 +1,26 @@
+"use client"
+import { useState } from 'react';
 import CorporationSection from "@/components/CorporationSection";
 import TitleBar from "@/components/TitleBar";
-import Image from "next/image";
+import EmailFormat from "@/components/EmailFormat"; // Import the EmailFormat component
 
 export default function Home() {
+  const [showEmailFormat, setShowEmailFormat] = useState(false); // State to toggle EmailFormat component
+  const [showCorporationSection, setShowCorporationSection] = useState(true); // State to toggle CorporationSection component
+
+  const handleShowEmailFormat = () => {
+    setShowEmailFormat((prevShowEmailFormat) => !prevShowEmailFormat);
+    setShowCorporationSection((prevShowCorporationSection) => !prevShowCorporationSection);
+  };
+
   return (
-  <main className="flex min-h-screen flex-col bg-[#121212] no-scrollbar rounded-xl">
-    <TitleBar/>
-    <div className="container mx-auto px-12 py-4 text-white">
-      <CorporationSection/>
-    </div>
-  </main>
+    <main className="flex min-h-screen flex-col bg-[#121212] no-scrollbar rounded-xl">
+      <TitleBar/>
+      <div className="container mx-auto px-12 py-4 text-white">
+      <button onClick={handleShowEmailFormat}>Show Email Format</button>
+        {showEmailFormat && <EmailFormat />} {/* Conditionally render EmailFormat component */}
+        {showCorporationSection && <CorporationSection/>}
+      </div>
+    </main>
   );
 }
